7b199a2cafc2e66f38209cf0cfbd770a3b8796d6
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-10-02 22:02:13 +0000

    feat: Add an implmentation of the collatz problem

---
+++ recursion/collatz.rb
@@ -0,0 +1,19 @@
+#!/urb/bin/env ruby
+# frozen_string_literal: true
+
+def collatz(num)
+  raise ArgumentError, 'n must be greater than zero' if num <= 0
+
+  return 0 if num == 1
+
+  return 1 + collatz(num / 2) if num.even?
+
+  1 + collatz((3 * num) + 1) if num.odd?
+end
+
+number = 10
+
+number.downto(1) do |n|
+  steps = collatz(n)
+  puts "It took #{steps} steps to go from #{n} to 1."
+end
